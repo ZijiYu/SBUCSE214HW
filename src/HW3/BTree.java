c9349b9204113af1be1dfaf8e51125f048fe6f9a
kenlevine <1@>
2023-11-28 21:30:11 -0500

    HW3

--- src/HW3/BTree.java
+++ src/HW3/BTree.java
@@ -1,9 +1,30 @@
 package HW3;
+import java.util.Collection.*;
+import java.util.LinkedList;
+
+class Node<E extends Comparable<E>>{
+    private E num;
+    private LinkedList<E> key;
+    private boolean isLeaf;
+    private LinkedList<Node<E>> children;
+
+    private Node(int degree, boolean isLeaf){
+        this.num = null;
+        this.isLeaf = true;
+        this.key = null;
+        this.children = null;
+    }
 
 public class BTree<E extends Comparable<E>> {
-    private boolean isLeaf;
-    private int height;
-    private Comparable key;
+        private Node<E> root;
+        private int degree;
+        public void BTree(int degree){
+            this.degree = degree;
+        }
+
+
+
+    }
 
 
 }
